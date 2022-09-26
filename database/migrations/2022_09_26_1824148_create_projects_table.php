871d71ee6c5ab0c0bf32abbbb4aebb36303fb6e9
Alberto Camilo Rodriguez <svg.z32@gmail.com>
2022-09-26 13:55:57 -0500

    creacion de migraciones

---
+++ database/migrations/2022_09_26_1824148_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->unique();
+            $table->foreignId('project_type_id')->nullable(false)->constrained('project_types');// FVC, DESA
+            $table->string('step_name', 50); // Implementacion, cierre, etc
+            $table->string('substep_name', 50); // registro, actividades
+            $table->integer('percentage')->nullable(); // registro, actividades
+            $table->json('content'); // Contenido de la seccion escogida
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
