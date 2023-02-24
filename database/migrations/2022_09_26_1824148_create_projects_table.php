4a23a61dd366acc93c83035c1774199fc435d8b9
ialbertocamilo <svg.z32@gmail.com>
2023-02-24 08:34:46 -0500

    fix projects migration

--- database/migrations/2022_09_26_1824148_create_projects_table.php
+++ database/migrations/2022_09_26_1824148_create_projects_table.php
@@ -17,7 +17,7 @@ return new class extends Migration {
             $table->id();
             $table->uuid()->unique();
             $table->foreignId('project_type_id')->default(1)->constrained('project_types');// FVC, DESA
-            $table->text('name');
+            $table->text('name')->nullable();
             $table->text('description')->nullable();
             $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
